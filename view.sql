23c84eef571ff06bafe37d310f950e0e974dce07
Shanto93 <shantoislam7363@gmail.com>
2025-05-21 11:38:53 +0600

    Created function in sql query

--- view.sql
+++ view.sql
@@ -1,7 +1,21 @@
 select * FROM employees2;
 
 CREATE VIEW department_avg_salary AS
-SELECT department_name, round(avg(salary)) FROM employees2
-GROUP BY department_name;
+SELECT department_name, round(avg(salary))
+FROM employees2
+GROUP BY
+    department_name;
 
 SELECT * FROM department_avg_salary;
+
+CREATE FUNCTION count_employee()
+RETURNS INTEGER
+LANGUAGE SQL
+AS
+$$
+    SELECT count(*) FROM employees2;
+$$
+
+SELECT * FROM count_employee ();
+
+SELECT count(*) FROM employees2;
